8a9e73a4dd740e6e8643f79911a5aff122b53b1d
Quynh Nhu Nguyen <118354457+LuciaNhuNguyen@users.noreply.github.com>
2024-02-01 20:25:10 +0100

    Update 2.validateSRA.sh

--- Download_raw_data/2.validateSRA.sh
+++ Download_raw_data/2.validateSRA.sh
@@ -7,7 +7,6 @@ CSV_FILE="path/to/samplesheet_.csv"
 
 # Read the list of files and sample names from the input CSV
 tail -n +2 "$CSV_FILE" | while IFS=',' read -r PATIENT SAMPLE LANE FASTQ_1 FASTQ_2; do
-    
     # Check if the renamed files exist
     if [ "$(find "$OUTPUT_DIR" -name "${SAMPLE}_*" | wc -l)" -eq 2 ]; then
         echo "Paired-end FASTQ ${SAMPLE} files have 2 reads each."
